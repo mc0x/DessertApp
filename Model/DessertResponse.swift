84bf1601b81bd5e63ea959fa496db0e1f8104709
Mayra Cortez <45056239+mc0x@users.noreply.github.com>
2023-06-27 16:19:13 -0700

    Add files via upload

---
+++ Model/DessertResponse.swift
@@ -0,0 +1,13 @@
+//
+//  DessertResponse.swift
+//  Dessert App
+//
+//  Created by Mayra Cortez on 6/25/23.
+//
+
+import Foundation
+
+struct DessertResponse: Codable, Hashable {
+    var meals: [Dessert]
+}
+
